5fed0f84000375733dcb153ad7a23830ef0ffb2a
manogarans <mgaran309@gmail.com>
2023-11-22 15:01:44 +0530

    first commit

--- dataproject/myapp/admin.py
+++ dataproject/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student,StudentAdmin,Employee,EmployeeAdmin
+
 
 # Register your models here.
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Employee,EmployeeAdmin)
--- dataproject/myapp/models.py
+++ dataproject/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib import admin
+
 
 # Create your models here.
+class Student (models.Model):
+    referencenumber=models.CharField(max_length=20,help_text="reference number")
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('referencenumber','name','age','email')
+class Employee (models.Model):
+    emp_id=models.CharField(primary_key=True,max_length=4,help_text='Employee ID')
+    ename=models.CharField(max_length=50)
+    post=models.CharField(max_length=20)
+    salary=models.IntegerField()
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('emp_id','ename','post','salary')
